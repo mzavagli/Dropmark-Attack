7bcb49e1fefed1028e21ad78520d2efd729fae9f
mzavagli <mike2024@hotmail.be>
2024-05-18 05:01:16 +0200

    Timing analysis

---
+++ shadow/examples/docs/thesis/analysis/compute_mean_times.py
@@ -0,0 +1,26 @@
+from statistics import mean, median
+
+LOGS_PATH = "logs/output_all.txt"
+
+with open(LOGS_PATH) as log_file:
+    logs = log_file.readlines()
+
+mean_before = []
+mean_after = []
+
+for i in range(len(logs)):
+    if "Spotted" in logs[i]:
+        circid = logs[i-1].split(" ")[-1]
+        tmp_list = []
+        j = i
+        while len(tmp_list) != 4:
+            if logs[j].split(" ")[-1] == circid:
+                tmp_list.append(int(logs[j].split(" ")[2].split(":")[-2])*60 + float(logs[j].split(" ")[2].split(":")[-1]))
+            j -= 1
+        tmp_list.append(int(logs[j].split(" ")[2].split(":")[-2])*60 + float(logs[j].split(" ")[2].split(":")[-1]))
+        print(tmp_list)
+        mean_after.append(tmp_list[0]-tmp_list[1])
+        mean_before.append(tmp_list[3]-tmp_list[4])
+
+print(f"BEFORE DROPMARK: Mean:{mean(mean_before)}, median:{median(mean_before)}, min:{min(mean_before)}, max:{max(mean_before)}")
+print(f"AFTER DROPMARK: Mean:{mean(mean_after)}, median:{median(mean_after)}, min:{min(mean_after)}, max:{max(mean_after)}")
--- shadow/examples/docs/thesis/analysis/extract_logs.py
+++ shadow/examples/docs/thesis/analysis/extract_logs.py
@@ -1,35 +1,40 @@
+EXTRACT_EXIT = False
+EXTRACT_RELAY = True
+
 number_of_relay = 12
 number_of_exit = 4
 
-OUTPUT_PATH = "logs/output.txt"
+OUTPUT_PATH = "logs/output_all.txt"
 
 content = ""
 
-for i in range(number_of_exit):
+if (EXTRACT_EXIT):
+    for i in range(number_of_exit):
 
-    EXIT_LOG_PATH = f"../shadow.data/hosts/exit{i+1}/tor.1000.stdout"
+        EXIT_LOG_PATH = f"../shadow.data/hosts/exit{i+1}/tor.1000.stdout"
 
-    content += f"--- EXIT{i+1} ---\n"
+        content += f"--- EXIT{i+1} ---\n"
 
-    with open(EXIT_LOG_PATH) as infile:
-        logs = infile.readlines()
+        with open(EXIT_LOG_PATH) as infile:
+            logs = infile.readlines()
 
-    for line in logs:
-        if "DROPMARK" in line:
-            content += line
+        for line in logs:
+            if "DROPMARK" in line:
+                content += line
 
-for i in range(number_of_relay):
+if (EXTRACT_RELAY):
+    for i in range(number_of_relay):
 
-    RELAY_LOG_PATH = f"../shadow.data/hosts/relay{i+1}/tor.1000.stdout"
+        RELAY_LOG_PATH = f"../shadow.data/hosts/relay{i+1}/tor.1000.stdout"
 
-    content += f"--- RELAY{i+1} ---\n"
+        content += f"--- RELAY{i+1} ---\n"
 
-    with open(RELAY_LOG_PATH) as infile:
-        logs = infile.readlines()
+        with open(RELAY_LOG_PATH) as infile:
+            logs = infile.readlines()
 
-    for line in logs:
-        if "Spotted" in line:
-            content += line
+        for line in logs:
+            if "DROPMARK" in line:
+                content += line
 
 with open(OUTPUT_PATH, "w") as outfile:
     outfile.write(content)
